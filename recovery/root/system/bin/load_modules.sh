5c9def96c9e78654b0c676e23fe117018f896d37
SudirEbi <sudheerk846@gmail.com>
2025-06-21 22:18:13 +0530

    update load_modules

--- recovery/root/system/bin/load_modules.sh
+++ recovery/root/system/bin/load_modules.sh
@@ -1,21 +1,74 @@
 #!/sbin/sh
 
-# Team Hovatek Touch Fix
-load_panel()
+load_touchscreen()
 {
-	insmod /lib/modules/gslX680.ko
-	insmod /lib/modules/adaptive-ts.ko
-	insmod /lib/modules/gpio.ko
-	insmod /lib/modules/gpio-sprd.ko
-	insmod /lib/modules/focaltech_ft8756_spi_ts.ko
-	insmod /lib/modules/nvt_nt36xxx_spi_ts.ko
-	insmod /lib/modules/fortsense_fp.ko
-	insmod /lib/modules/sc27xx_tsensor_thermal.ko
-	insmod /lib/modules/tran_gesture.ko
+    insmod /lib/modules/focaltech_ft8756_spi_ts.ko
+    insmod /lib/modules/nvt_nt36xxx_spi_ts.ko
+    insmod /lib/modules/focaltech_tp.ko
+    insmod /lib/modules/focaltech_touch_modules.ko
+    insmod /lib/modules/chipone_tddi.ko
 }
 
+load_gpio()
+{
+    insmod /lib/modules/gpio.ko
+    insmod /lib/modules/gpio-sprd.ko
+    insmod /lib/modules/gpio-pmic-eic-sprd.ko
+    insmod /lib/modules/gpio-eic-sprd.ko
+    insmod /lib/modules/pinctrl.ko
+    insmod /lib/modules/pinctrl-sprd.ko
+    insmod /lib/modules/pinctrl-sprd-qogirl6.ko
+}
 
-load_panel
-wait 1
+load_trusty()
+{
+    insmod /lib/modules/trusty.ko
+    insmod /lib/modules/trusty_log.ko
+    insmod /lib/modules/trusty_pm.ko
+    insmod /lib/modules/trusty_irq.ko
+    insmod /lib/modules/trusty_virtio.ko
+    insmod /lib/modules/trusty-ipc.ko
+    insmod /lib/modules/trusty-tui.ko
+}
+
+load_usb()
+{
+    insmod /lib/modules/usb_f_vser.ko
+    insmod /lib/modules/extcon-usb-gpio.ko
+    insmod /lib/modules/musb_sprd.ko
+    insmod /lib/modules/musb_hdrc.ko
+}
+
+load_audio()
+{
+    insmod /lib/modules/audio_pipe.ko
+    insmod /lib/modules/audio_mem.ko
+    insmod /lib/modules/audio_sipc.ko
+    insmod /lib/modules/audio-dsp-dump.ko
+}
+
+load_misc()
+{
+    insmod /lib/modules/sc27xx_tsensor_thermal.ko
+    insmod /lib/modules/hardware_info.ko
+    insmod /lib/modules/device_info.ko
+    insmod /lib/modules/sensorhub.ko
+    insmod /lib/modules/kfifo_buf.ko
+    insmod /lib/modules/ion_cma_heap.ko
+    insmod /lib/modules/ion_ipc_trusty.ko
+    insmod /lib/modules/sprd_ion.ko
+}
+
+main()
+{
+    load_touchscreen
+    load_gpio
+    load_trusty
+    load_usb
+    load_audio
+    load_misc
+}
+
+main
 setprop modules.loaded 1
 exit 0
